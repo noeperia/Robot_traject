805f7468e90cf8e85a39211bda4e14aa4a375405
merguez31 <102671995+merguez31@users.noreply.github.com>
2022-03-31 14:11:54 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ from tkinter import *
 ##----- Créations des Fonctions -----##
 def choisirformes(event):
     if formeee.get() == 1:
-        rect1 = dessin.create_rectangle(event.x, event.y, event.x-100, event.y-50, width=2, outline='#d05e82')
+        rect1 = dessin.create_rectangle(event.x+100, event.y+40, event.x-100, event.y-50, fill='black', width=3, outline='red')
     elif formeee.get() == 2:
-        carr = dessin.create_rectangle(event.x, event.y, event.x-47, event.y-47, width=2, outline='#d05e82')
+        carr = dessin.create_rectangle(event.x+40, event.y+40, event.x-47, event.y-47, fill='black', width=3, outline='red')
     elif formeee.get() == 3:
-        cercle = dessin.create_oval(event.x, event.y, event.x-47, event.y-47, fill='orange', outline='blue')
+        cercle = dessin.create_oval(event.x+40, event.y+40, event.x-47, event.y-47, fill='black', width=3, outline='red')
     else:
         formeee.set(0)
 
@@ -44,26 +44,33 @@ dessin.grid(row = 0, column = 0, columnspan = 2, padx = 3, pady = 3)
 dessin2 = Canvas(fen, width = 400, height = 858, bg = 'white')
 dessin2.grid(row = 0, column = 40, columnspan = 1, padx = 3, pady = 3)
 
+e1 = Entry(dessin2)
+dessin2.create_window(260,500,window=e1, height=500, width=250)
+
 
 ##----- Création des boutons -----##
 formeee = IntVar()
 
-bouton_quitter = Button(fen, text='Quitter', command=fen.destroy)
-bouton_quitter.grid(row = 1, column = 1, padx = 3, pady = 3, sticky=E)
-
 bouton_rectangle = Button(fen, text='Rectangle', command=rec)
-bouton_rectangle = dessin2.create_window(100,150, window = bouton_rectangle)
+bouton_rectangle = dessin2.create_window(70,110, window = bouton_rectangle)
 
 bouton_carre = Button(fen, text='Carré', command=car)
-bouton_carre = dessin2.create_window(100,250, window = bouton_carre)
+bouton_carre = dessin2.create_window(70,150, window = bouton_carre)
 
 bouton_rond = Button(fen, text='Rond', command=ro)
-bouton_rond = dessin2.create_window(100,320, window = bouton_rond)
+bouton_rond = dessin2.create_window(70,190, window = bouton_rond)
+
+##----- Création du Label -----##
+lbl = Label(dessin2, text='',fg='black',bg='black')
+
 
 ##----- Dessiner dans le canevas -----##
 # TEXTE
-message = Label(fen, text='Ici du texte.')
-message.grid(row=8, column=0, columnspan=2, padx=3, pady=3, sticky=W+E)
+message = Label(fen, text='Souris Temps Réel')
+message.grid(row=1, column=0, columnspan=2, padx=0, pady=0, sticky=W+E)
+
+bouton_quitter = Button(fen, text='Quitter', command=fen.destroy)
+bouton_quitter.grid(row = 1, column = 1, padx = 3, pady = 3, sticky=E)
 
 # Image 
 im = PhotoImage(file = '/home/robot/Bureau/NoeSacha/carte_projet.png', master=fen)
